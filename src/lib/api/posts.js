be51a9d516b39981460f0d668f46c58d280026c1
koreanddinghwan <myunghwan0421@gmail.com>
2021-12-30 22:57:13 +0900

    26.1.1

--- src/lib/api/posts.js
+++ src/lib/api/posts.js
@@ -3,3 +3,6 @@ import client from './client';
 
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
+
+//id로 서버에 get 요청 날리기
+export const readPost = (id) => client.get(`/api/post/${id}`);
